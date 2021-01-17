7fcd1d1a3ceda4837a82305f161185be61751793
Anil Sharma <anilsharma@Anils-MacBook-Air.local>
2021-01-17 15:51:33 +0530

    removed nuxt image plugin

--- pages/index.vue
+++ pages/index.vue
@@ -68,7 +68,7 @@
                 <a href="#" class="text-blue-500 hover:underline">Read more</a>
                 <div>
                   <a href="#" class="flex items-center">
-                    <nuxt-img
+                    <img
                       src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
                       alt="avatar"
                       class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
@@ -106,7 +106,7 @@
                 <a href="#" class="text-blue-500 hover:underline">Read more</a>
                 <div>
                   <a href="#" class="flex items-center">
-                    <nuxt-img
+                    <img
                       src="https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=333&amp;q=80"
                       alt="avatar"
                       class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
@@ -144,7 +144,7 @@
                 <a href="#" class="text-blue-500 hover:underline">Read more</a>
                 <div>
                   <a href="#" class="flex items-center">
-                    <nuxt-img
+                    <img
                       src="https://images.unsplash.com/photo-1531251445707-1f000e1e87d0?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=281&amp;q=80"
                       alt="avatar"
                       class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
@@ -182,7 +182,7 @@
                 <a href="#" class="text-blue-500 hover:underline">Read more</a>
                 <div>
                   <a href="#" class="flex items-center">
-                    <nuxt-img
+                    <img
                       src="https://images.unsplash.com/photo-1500757810556-5d600d9b737d?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=735&amp;q=80"
                       alt="avatar"
                       class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
@@ -220,7 +220,7 @@
                 <a href="#" class="text-blue-500 hover:underline">Read more</a>
                 <div>
                   <a href="#" class="flex items-center">
-                    <nuxt-img
+                    <img
                       src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=373&amp;q=80"
                       alt="avatar"
                       class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
@@ -266,7 +266,7 @@
             <div class="flex flex-col bg-white max-w-sm px-6 py-4 mx-auto rounded-lg shadow-md">
               <ul class="-mx-4">
                 <li class="flex items-center">
-                  <nuxt-img
+                  <img
                     src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
                     alt="avatar"
                     class="w-10 h-10 object-cover rounded-full mx-4"
@@ -277,7 +277,7 @@
                   </p>
                 </li>
                 <li class="flex items-center mt-6">
-                  <nuxt-img
+                  <img
                     src="https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=333&amp;q=80"
                     alt="avatar"
                     class="w-10 h-10 object-cover rounded-full mx-4"
@@ -288,7 +288,7 @@
                   </p>
                 </li>
                 <li class="flex items-center mt-6">
-                  <nuxt-img
+                  <img
                     src="https://images.unsplash.com/photo-1531251445707-1f000e1e87d0?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=281&amp;q=80"
                     alt="avatar"
                     class="w-10 h-10 object-cover rounded-full mx-4"
@@ -299,7 +299,7 @@
                   </p>
                 </li>
                 <li class="flex items-center mt-6">
-                  <nuxt-img
+                  <img
                     src="https://images.unsplash.com/photo-1500757810556-5d600d9b737d?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=735&amp;q=80"
                     alt="avatar"
                     class="w-10 h-10 object-cover rounded-full mx-4"
@@ -310,7 +310,7 @@
                   </p>
                 </li>
                 <li class="flex items-center mt-6">
-                  <nuxt-img
+                  <img
                     src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=373&amp;q=80"
                     alt="avatar"
                     class="w-10 h-10 object-cover rounded-full mx-4"
@@ -398,7 +398,7 @@
               </div>
               <div class="flex justify-between items-center mt-4">
                 <div class="flex items-center">
-                  <nuxt-img
+                  <img
                     src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
                     alt="avatar"
                     class="w-8 h-8 object-cover rounded-full"
