b9d934ec6ce8a08a68c03d1f52dcaf0371ee97ea
Anil Sharma <anilsharma@Anils-MacBook-Air.local>
2021-01-23 23:04:47 +0530

    configurations & some progress on theme switch

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,12 @@ import { modules, buildModules, modulesSettings } from './config/modules'
 import plugins from './config/plugins'
 
 export default {
+  serverMiddleware: [
+    // API middleware
+    "./config/head",
+    "./config/modules",
+    "./config/plugins"
+  ],
   head: head,
   css: [],
   plugins: plugins,
