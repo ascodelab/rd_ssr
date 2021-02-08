b5a3fef243bea4192eac75dba9da5f7ae2766346
Anil Sharma <anilsharma@Anils-MacBook-Air.local>
2021-02-08 22:14:56 +0530

    vuex added for seo

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,52 @@
     <Footer />
   </div>
 </template>
+<script>
+export default {
+  head() {
+    return {
+      title: this.$store.state.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.$store.state.subtitle
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: this.$store.state.subtitle
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: this.$store.state.title
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: this.$store.state.featureImage
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: this.$store.state.url
+        },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: `summary_large_image`
+        },
+        {
+          hid: "og:site_name",
+          name: "og:site_name",
+          content: "Rati Digital"
+        }
+      ]
+    };
+  }
+};
+</script>
 <style lang="postcss">
 .dark-mode {
   @apply bg-gray-800 text-gray-100;
--- store/index.js
+++ store/index.js
@@ -1,3 +1,23 @@
 export const state = () => ({
     theme: 'default',
+    url: 'http://localhost:8080/',
+    title: 'asdads',
+    subtitle: 'asdadssas',
+    featureImage: 'http://localhost:8080/uploads/anil-sharma.jpg',
 })
+
+//setting data [ mutations]
+export const mutations = {
+    set(state, data) {
+        state = Object.assign(state, data)
+    }
+}
+
+// action to be called 
+
+export const actions = {
+    setSEO(context, payload) {
+        context.commit('set', payload)
+    }
+}
+
