43794af1edc48378deb60b10f87665c154cb6846
Anil Sharma <anilsharma@Anils-MacBook-Air.local>
2021-02-10 16:02:09 +0530

    json+ld added for article pages

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -13,7 +13,17 @@
 }
 </style>
 <script>
+import { getFormattedDate } from "~/utils/helper";
+
 export default {
+  data() {
+    return {};
+  },
+  computed: {
+    datePretty() {
+      return getFormattedDate(this.article.date);
+    }
+  },
   async asyncData({ $content, params }) {
     const article = await $content("posts", params.slug).fetch();
     return {
@@ -32,6 +42,37 @@ export default {
       data.url = `${this.$config.baseURL}${this.article.slug}`;
       this.$store.dispatch("setSEO", data);
     }
+  },
+  head() {
+    let structuredData = {
+      "@context": "https://schema.org",
+      "@type": "BlogPosting",
+      mainEntityOfPage: {
+        "@type": "WebPage",
+        "@id": `${this.$config.baseURL}${this.article.slug}`
+      },
+      headline: this.article.title,
+      description: this.article.subtitle,
+      image: `${this.$config.baseURL}${this.article.featureImage}`,
+      author: {
+        "@type": "Person",
+        name: this.article.author
+      },
+      publisher: {
+        "@type": "Organization",
+        name: "Rati Digital",
+        logo: {
+          "@type": "ImageObject",
+          url: "https://ratidigital.in/img/logo-white.png"
+        }
+      },
+      datePublished: this.datePretty,
+      dateModified: this.datePretty
+    };
+    //set rich-snippet data
+    return {
+      script: [{ type: "application/ld+json", json: structuredData }]
+    };
   }
 };
 </script>
