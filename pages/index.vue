73ef652765ec145a86839c351e78e3ceb07bae4e
Anil Sharma <anilsharma@Anils-MacBook-Air.local>
2021-02-10 15:22:56 +0530

    json+ld added for landing page

--- pages/index.vue
+++ pages/index.vue
@@ -23,10 +23,23 @@ export default {
         url: this.$config.baseURL
       },
       structuredData: {
-        "@context": "http://schema.org",
-        "@type": "website",
+        "@context": "http://www.schema.org",
+        "@type": "EducationalOrganization",
         name: "Rati Digital",
-        url: "https://ratidigital.in"
+        url: "https://ratidigital.in",
+        logo: "https://ratidigital.in/img/logo-white.png",
+        image: "https://ratidigital.in/img/logo-white.png",
+        description: "E-learning and training institute.",
+        address: {
+          "@type": "PostalAddress",
+          streetAddress: "53, village hargangoli",
+          addressLocality: "joura",
+          addressRegion: "madhya pradesh",
+          postalCode: "476221",
+          addressCountry: "india"
+        },
+        openingHours:
+          "Mo 09:00-18:00 Tu 09:00-18:00 We 09:00-18:00 Th 09:00-18:00 Fr 09:00-18:00"
       }
     };
   },
