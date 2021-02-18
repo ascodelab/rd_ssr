489e11e23eeb6d5e4f68038b15f59547d3bdb661
Anil Sharma <anilsharma@Anils-MacBook-Air.local>
2021-02-18 19:33:27 +0530

    card loader added

--- components/homepage/ArticleList.vue
+++ components/homepage/ArticleList.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="w-al">
     <div class="grid-config">
-      <card-loader v-for="i in 3" :key="i"></card-loader>
       <article v-for="(article, index) in articleList" :key="index">
         <post-card
           :postTitle="article.title"
@@ -15,13 +14,18 @@
           :postContent="article.body"
         ></post-card>
       </article>
+      <!-- card loader -->
+      <div v-for="i in 3" :key="i*Math.random()">
+        <card-loader v-if="fetching"></card-loader>
+      </div>
+      <!-- observer loading -->
       <div v-observe-visibility="handleScrolledToBottom"></div>
     </div>
   </div>
 </template>
 <style lang="postcss" scoped>
 .w-al {
-  @apply flex justify-center mx-4 my-16;
+  @apply flex justify-center w-full mx-4 mt-16;
 }
 .grid-config {
   @apply grid md:grid-cols-2 lg:grid-cols-3 gap-y-10 gap-x-6 flex-1;
@@ -39,6 +43,8 @@ export default {
         .skip(this.skip)
         .limit(3)
         .fetch();
+      //hide card loader on complete
+      this.fetching = false;
       if (response.length === 0) {
         this.lastPage = true;
         return;
@@ -54,11 +60,14 @@ export default {
       if (this.lastPage) return false;
       this.page++;
       this.skip += 3;
+      //enable card loader
+      this.fetching = true;
       this.fetchData();
     }
   },
   data() {
     return {
+      fetching: true,
       articleList: [],
       page: 1,
       skip: 0,
