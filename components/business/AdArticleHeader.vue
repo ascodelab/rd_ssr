1da1df2a46e6ad9701f450ba7190436c6b59f2ab
Anil Sharma <anilsharma@Anils-MacBook-Air.local>
2021-02-23 11:31:06 +0530

    google ad placed

--- components/business/AdArticleHeader.vue
+++ components/business/AdArticleHeader.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <style lang="postcss" scoped>
-.wrapper {
+.wrapper- {
   @apply flex items-center justify-center prose prose-sm mx-auto p-2 max-h-56 overflow-hidden mt-2;
 }
 </style>
